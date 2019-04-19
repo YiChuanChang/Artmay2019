42e7eccd6f3b33b56b4e2862f28f4ff5c706db39
Fiona Huang <fionahuang0915@gmail.com>
2019-04-19 21:10:54 +0800

    未用slide的sync5之前push一下

--- js/index.js
+++ js/index.js
@@ -22,27 +22,31 @@ function scroll_to_squares(num) {
 function click1(num) {
     switch(num) {
         case 1:
-            show_detail.style.display = "block";
+            //show_detail.style.display = "block";
             show_detail.innerHTML = '<img src="imgs/index/工作坊橫幅2.png" alt="工作坊橫幅2" title="工作坊橫幅2">';
             break;
         case 2:
-            show_detail.style.display = "block";
+            //show_detail.style.display = "block";
             show_detail.innerHTML = '<img src="imgs/index/工作坊橫幅3.png" alt="工作坊橫幅3" title="工作坊橫幅3">';            
             break;
         case 3:
-            show_detail.style.display = "block";
+            //show_detail.style.display = "block";
             show_detail.innerHTML = '<img src="imgs/index/直撥快樂.png" alt="直撥快樂" title="直撥快樂">';
             break;
         case 4:
-            show_detail.style.display = "block";
+            //show_detail.style.display = "block";
             show_detail.innerHTML = '<img src="imgs/index/解壓縮.png" alt="解壓縮" title="解壓縮">';
             break;
         case 5:
-            show_detail.style.display = "block";
-            show_detail.innerHTML = '<img src="imgs/index/前夜祭.png" alt="前夜祭" title="前夜祭">';            
+            //show_detail.style.display = "block";
+            show_detail.innerHTML = '<img src="imgs/index/廢泡橫幅.png" alt="廢泡橫幅" title="廢泡橫幅">';            
             break;
         case 6:
-            show_detail.style.display = "block";
+            //show_detail.style.display = "block";
+            show_detail.innerHTML = '<img src="imgs/index/前夜祭.png" alt="前夜祭" title="前夜祭">';            
+            break;
+        case 7:
+            //show_detail.style.display = "block";
             show_detail.innerHTML = '<img src="imgs/index/工作坊橫幅1.png" alt="工作坊橫幅1" title="工作坊橫幅1">';            
             break;
 
